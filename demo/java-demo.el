92b5f1ce50a9decd4b5206d0e0334ca224253c4c
Carl Olson <carl-olson@uiowa.edu>
2015-07-15 23:57:33 -0500

    update java demo with infjava

--- demo/java-demo.el
+++ demo/java-demo.el
@@ -1,8 +1,9 @@
 
-(load-file "../se-helpers.el")
 (load-file "../se.el")
+(load-file "../se-helpers.el")
+(load-file "../se-mode.el")
+(load-file "../se-inf.el")
 
-(defconst json-array-type 'list)
 (load-file "../json.el/json.el")
 
 (defun list-to-spans (list)
@@ -10,11 +11,19 @@
 	  list))
 
 (defconst se-mode-demo-tree
-  (se-create-parse-tree
-   (list-to-spans (json-read-file "java_spans"))))
+  (let ((json-array-type 'list))
+    (se-create-parse-tree
+     (list-to-spans (json-read-file "java_spans")))))
 
-(load-file "../se-mode.el")
+(defun se-mode-parse-file ()
+  (interactive)
+  (se-inf-ask (concat "PARSE-FILE\t" (buffer-file-name) "\tjava")))
+(define-key se-mode-map (kbd "C-c c") #'se-mode-parse-file)
 
 (find-file "InfJava.java")
 (se-mode)
+(setq se-inf-process
+      (start-process "java-demo" "*se-mode: java-demo*"
+		     "java" "-cp" "*" "InfJava"))
+(se-inf-start)
 (setq se-mode-parse-tree se-mode-demo-tree)
