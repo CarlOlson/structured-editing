f57e9ae771005f496e4f98870f4797681861b67d
Carl Olson <carl-olson@uiowa.edu>
2015-12-07 23:55:10 -0600

    update se-create-mode and add documentation

--- se-macros.el
+++ se-macros.el
@@ -1,22 +1,44 @@
 
-(defmacro se-create-mode (mode parent &rest body)
-  ""
+(defmacro se-create-mode (name parent &rest body)
+  "A macro for defining structured editing based modes.  This
+macro should contain best practices simply at the cost of
+customization.
+
+NAME should be your mode's name, properly capitalized.
+PARENT should be either nil or a mode to derive the new mode
+from.
+BODY should contain any code to be executed when the mode starts.
+It is expected that `se-inf-start' is called.
+
+Example:
+  (se-create-mode \"Ruby\" ruby-mode
+    (se-inf-start ...))"
   (declare (indent 2) (debug t))
-  `(progn
-     (define-derived-mode
-       ,(intern (downcase (format "se-%s-mode" mode)))
-       ,(or parent
-	    ;; keep emacs version <24 compatability
-	    (if (fboundp 'prog-mode) 'prog-mode 'fundamental-mode))
-       ,(format "se-%s" mode)
-       ,(format "Major mode for %s files." mode)
-       (se-mode)
-       (add-hook 'se-navigation-mode-hook
-		 ',(intern (format "se-%s-parse-file" mode)) nil t)
-       ,@body)
-     
-     (defun ,(intern (format "se-%s-parse-file" mode)) ()
-       (when se-navigation-mode
-	 (se-inf-parse-file)))))
+  (cl-macrolet ((idf (&rest args)
+		     `(intern (downcase (format ,@args)))))
+    `(progn
+       (define-derived-mode
+	 ,(idf "se-%s-mode" name)
+	 ,(or parent
+	      ;; keep emacs version <24 compatability
+	      (if (fboundp 'prog-mode) 'prog-mode 'fundamental-mode))
+	 ,(format "se-%s" name)
+	 ,(format "Major mode for %s files." name)
+	 (se-mode)
+	 (add-hook 'se-navigation-mode-hook
+		   ',(idf "se-%s-parse-file" name) nil t)
+	 ,@body)
+
+       (if (not (fboundp ',(idf "%s-mode" name)))
+	   (defalias
+	     ',(idf "%s-mode" name)
+	     ',(idf "se-%s-mode" name)))
+       
+       (defun ,(idf "se-%s-parse-file" name) ()
+	 "Only parses when navigation mode is active. This prevents
+the navigation mode hook from calling `se-inf-parse-file' when
+deactivating."
+	 (when se-navigation-mode
+	   (se-inf-parse-file))))))
 
 (provide 'se-macros)
